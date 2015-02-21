b17b2fa7d3fe8298fc8ffd6e7e4797b4b085e892
SAM <snaden@macalester.edu>
2015-02-20 22:49:35 -0600

    starting server

--- server/test/app.js
+++ server/test/app.js
@@ -1,11 +1,13 @@
-/*global describe, it */
-'use strict';
-var assert = require('assert');
-var dataWhisperers = require('../');
+// /*global describe, it */
+// 'use strict';
+// var assert = require('assert');
+// var dataWhisperers = require('../');
 
-describe('data-whisperers node module', function () {
-  it('must have at least one test', function () {
-    dataWhisperers();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
-  });
-});
+// describe('data-whisperers node module', function () {
+//   it('must have at least one test', function () {
+//     dataWhisperers();
+//     assert(false, 'I was too lazy to write any tests. Shame on me.');
+//   });
+// });
+
+
