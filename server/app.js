b01ce68b5a3fa4e59ae1ba4b5b411c0107b99c4a
SAM <snaden@macalester.edu>
2015-02-20 23:21:36 -0600

    server is working!
    just serving up files for now

--- server/app.js
+++ server/app.js
@@ -22,12 +22,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(express.static(path.join(__dirname, 'public')));  //automatically create route for everything in public
 
+app.get('/', function (req, res, next) {
+	//res.sendFile(path.join(__dirname, '../Macman/public', 'index.html'))
+	res.sendFile(path.join(__dirname, '../front/www', 'index.html'))
+	//res.send("hello bro");
+})
+
 
 var server = app.listen(8080, function () {
-
   var host = server.address().address
   var port = server.address().port
-
   console.log('leezy listening at http://%s:%s', host, port)
-
 })
