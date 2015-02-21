f301cbc37f6a622aabbf595ddc3b81ed44722bca
Benjamin Hillmann <bhillmann@outlook.com>
2015-02-21 01:51:08 -0600

    Lastfm is done.

--- server/lastfm.js
+++ server/lastfm.js
@@ -2,6 +2,7 @@
  * Created by bhillmann on 21/02/15.
  */
 var LastFmNode = require('lastfm').LastFmNode;
+var _ = require('underscore');
 
 var lastfm = new LastFmNode({
     api_key: '9d693d889eb0898357d4121f009e5ef9',    // sign-up for a key at http://www.last.fm/api
@@ -11,16 +12,20 @@ var lastfm = new LastFmNode({
 var test = lastfm.request('user.getTopTracks', {
     user: 'bhillmann',
     handlers: {
-        success: processData,
+        success: function(data) {
+            var processed = _.map(data.toptracks.track, processData);
+            console.log(processed);
+        },
         error: function(error) {
             console.log("Error: " + error.message);
         }
     }
 });
 
-function processData(data) {
-    for (trackName in data.toptracks.track) {
-        console.log(data.toptracks.track[trackName]);
-        
-    }
+function processData(value, key) {
+    var processed = {};
+    processed.songName = value.name;
+    processed.artist = value.artist.name;
+    processed.dateLiked = "Feb. 21 2015";
+    return processed;
 };
